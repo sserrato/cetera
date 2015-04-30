aa80c0d4ffdd62a9a788605fd9c6f3f7bf44aec6
Marc Millstone <marc.millstone@socrata.com>
2015-04-29 17:06:45 -0700

    WIP. Proves that custom scorers can work

--- src/main/scala/com/socrata/cetera/search/ElasticSearchClient.scala
+++ src/main/scala/com/socrata/cetera/search/ElasticSearchClient.scala
@@ -7,11 +7,13 @@ import org.elasticsearch.client.Client
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.settings.ImmutableSettings
 import org.elasticsearch.common.transport.InetSocketTransportAddress
-import org.elasticsearch.index.query.{FilterBuilders, QueryBuilders}
+import org.elasticsearch.index.query.{FilterBuilders, QueryBuilders }
+import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders
 import org.elasticsearch.search.aggregations.AggregationBuilders
 import org.elasticsearch.search.aggregations.bucket.terms.Terms
 import org.elasticsearch.search.sort.{SortBuilders, SortOrder}
 
+import scala.collection.JavaConverters._
 import com.socrata.cetera.types.CeteraFieldType
 import com.socrata.cetera.types._
 import EnrichedFieldTypesForES._
@@ -76,12 +78,16 @@ class ElasticSearchClient(host: String, port: Int, clusterName: String) extends 
         matchQuery
       }
     }
-
+    
+    val custom = QueryBuilders.functionScoreQuery(query).boostMode("replace")
+    val script = ScoreFunctionBuilders.scriptFunction("cetera-ranker", "native", Map("boostLastUpdatedAtValue"-> 2.0, "boostPopularityValue" -> 2.0).asInstanceOf[Map[String,Object]].asJava)
+    custom.add(script)
     // Imperative builder --> order is important
+   println(custom.toString) 
     client
       .prepareSearch("datasets", "pages") // literals should not be here
       .setTypes(only.toList:_*)
-      .setQuery(query)
+      .setQuery(custom)
   }
 
   def buildSearchRequest(searchQuery: Option[String],
