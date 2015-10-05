d6bcdc9c855b521d35fe7e8a4a71b7b93091a1b1
John Krah <john.krah@socrata.com>
2015-10-05 12:00:28 -0700

    snapshot is optional

--- src/test/scala/com/socrata/cetera/services/VersionServiceSpec.scala
+++ src/test/scala/com/socrata/cetera/services/VersionServiceSpec.scala
@@ -9,7 +9,7 @@ class VersionServiceSpec extends WordSpec with ShouldMatchers {
       val resource = VersionService.version
           resource.name should be ("cetera")
           // Heads up, regex! This does NOT capture all valid versions (e.g., 1.2beta1)
-          resource.version should fullyMatch regex """\d+(\.\d+){0,2}(-SNAPSHOT)"""
+          resource.version should fullyMatch regex """\d+(\.\d+){0,2}(-SNAPSHOT)?"""
           resource.scalaVersion should be ("2.10.5")
     }
   }
