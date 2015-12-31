edb25fcbb820d96ac2a77db77d73f3c8ca553ae1
John Krah <john.krah@socrata.com>
2015-12-31 15:49:37 -0800

    optimize param conversion

--- src/main/scala/com/socrata/cetera/util/ParamConverter.scala
+++ src/main/scala/com/socrata/cetera/util/ParamConverter.scala
@@ -1,33 +1,32 @@
 package com.socrata.cetera.util
 
+import scala.util.control.NonFatal
+
 trait ParamConverter[T] {
   def convertFrom(s: String): Either[ParamConversionFailure, T]
+
+  private def convertFrom(s: String, f: (String => T)): Either[ParamConversionFailure, T] =
+    try {
+      Right(f(s))
+    } catch {
+      case NonFatal(_) => Left(InvalidValue(s))
+    }
 }
 
 object ParamConverter {
   implicit object IntParam extends ParamConverter[Int] {
-    override def convertFrom(s: String): Either[ParamConversionFailure, Int] = {
-      try { Right(s.toInt) }
-      catch { case e: Exception => Left(InvalidValue(s)) }
-    }
+    override def convertFrom(s: String): Either[ParamConversionFailure, Int] = convertFrom(s, _.toInt)
   }
 
   implicit object FloatParam extends ParamConverter[Float] {
-    override def convertFrom(s: String): Either[ParamConversionFailure, Float] = {
-      try { Right(s.toFloat) }
-      catch { case e: Exception => Left(InvalidValue(s)) }
-    }
+    override def convertFrom(s: String): Either[ParamConversionFailure, Float] = convertFrom(s, _.toFloat)
   }
 
   def filtered[A: ParamConverter, B](f: A => Option[B]): ParamConverter[B] = {
     new ParamConverter[B] {
       def convertFrom(s: String): Either[ParamConversionFailure, B] = {
         implicitly[ParamConverter[A]].convertFrom(s) match {
-          case Right(a) =>
-            f(a) match {
-              case Some(b) => Right(b)
-              case None => Left(InvalidValue(s))
-            }
+          case Right(a) => f(a).map(Right(_)).getOrElse(Left(InvalidValue(s)))
           case Left(e) => Left(e)
         }
       }
